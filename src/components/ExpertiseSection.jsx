2e60ac2e31016bdf90903e83fad50b473d84cc31
joeltech1807 <joelofficial18@gmail.com>
2026-05-03 17:33:46 +0530

    Rename 'Learn More' to 'Our Products' in Services section

--- src/components/ExpertiseSection.jsx
+++ src/components/ExpertiseSection.jsx
@@ -135,7 +135,7 @@ export default function ExpertiseSection() {
                   )}
                 </div>
                 <Link to={svc.link} className="expertise-card-link">
-                  Learn More
+                  Our Products
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                     <path d="M5 12h14M12 5l7 7-7 7" />
                   </svg>
